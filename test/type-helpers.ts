815cc789943dda67f224880d821092eb65b2a06f
Adolfo Marinucci <adospace@msn.com>
2018-03-04 23:11:03 +0100

    test getPropertyValue

--- test/type-helpers.ts
+++ test/type-helpers.ts
@@ -1,32 +1,49 @@
-/* import { capitalizeFirstLetter } from '../lib/index'
 import { expect } from 'chai';
 import 'mocha';
 
-describe('Hello function', () => {
-
-  it('should return hello world', () => {
-    const result = capitalizeFirstLetter("test!");
-    expect(result).to.equal('Test!');
-  });
-
-}); */
-import { expect } from 'chai';
-import 'mocha';
-
-import { getTypeName  } from '../lib/utils'
+import { getTypeName, getFirstAnchestor, getPropertyValue  } from '../lib/utils'
 
 class A {
-
+  get baseProperty(): string {
+      return "A.basePropertyValue";
+  }
 }
 
 class B extends A {
-
+  constructor() {
+    super();
+  }
+  get property(): string {
+    return "B.propertyValue";
+}
 }
 
 describe('getTypeName', () => {
-  it('it should return A', () => {
-    const result = getTypeName(new A());
-    expect(result).to.equal('A');
+  it('it should return B', () => {
+    const result = getTypeName(new B());
+    expect(result).to.equal('B');
   });
 
+});
+
+describe('getFirstAnchestor', () => {
+  it("it should return A with property 'baseProperty'", () => {
+    const result = getFirstAnchestor(new B());
+    expect(result).to.have.property('baseProperty');
+  });
+});
+
+describe('getPropertyValue', () => {
+  it("it should return B.propertyValue", () => {
+    const result = getPropertyValue(new B(), "property");
+    expect(result).to.equal('B.propertyValue');
+  });
+});
+
+
+describe('getPropertyValue', () => {
+  it("it should return A.basePropertyValue", () => {
+    const result = getPropertyValue(getFirstAnchestor(new B()), "baseProperty");
+    expect(result).to.equal('A.basePropertyValue');
+  });
 });
